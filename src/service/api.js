ea7a76f881057856f7f39ed6b587ff779e6082e2
Zaiynab Mansuri <53862415+ZaiMansuri@users.noreply.github.com>
2022-09-07 13:42:24 +0530

    update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,60 +1,33 @@
-import Axios from "axios";
-const defaultAxios = Axios.create({
-  headers: {},
-});
-defaultAxios.interceptors.response.use(
-  (res) => {
-    return res.data;
-  },
-  (err) => {
-    return Promise.reject(err);
-  }
-);
+import { apis } from "service";
 
-export const getTrackslimit5 = ({
-  data = {},
-  headers = {},
-  params = {},
-  path = {},
-} = {}) => {
-  return defaultAxios({
-    url: `https://api.spotify.com/v1/me/top/tracks?limit=5`,
-    method: "get",
-    params: params,
-    headers,
-    data,
-  });
+const COMMON_URL = `https://`;
+
+const API_URLS = {
+  GET_TRACKSLIMIT5: `${COMMON_URL}api.spotify.com/v1/me/top/tracks?limit=5`,
+  GET_RECOMMENDATIONSLIMIT20SEEDGENRESSADMARKET_I_NMINPOPULARITY10: `${COMMON_URL}api.spotify.com/v1/recommendations?limit=20&seed_genres=sad&market=IN&min_popularity=10`,
+  POST_TOKEN: `${COMMON_URL}accounts.spotify.com/api/token`,
 };
-export const getRecommendationslimit20seedgenressadmarketINminpopularity10 = ({
-  data = {},
-  headers = {},
-  params = {},
-  path = {},
-} = {}) => {
-  return defaultAxios({
-    url: `https://api.spotify.com/v1/recommendations?limit=20&market=IN&min_popularity=10`,
-    method: "get",
-    params,
-    headers,
-    data,
+
+export const getTrackslimit5 = (payload) =>
+  apis.get(API_URLS.GET_TRACKSLIMIT5, {
+    ...payload,
+    params: { time_range: "{{timeRange}}", ...payload?.params },
   });
-};
-export const postToken = ({
-  data = {},
-  headers = {},
-  params = {},
-  path = {},
-} = {}) => {
-  return defaultAxios({
-    url: `https://accounts.spotify.com/api/token`,
-    method: "post",
-    params,
+
+export const getRecommendationslimit20seedgenressadmarketINminpopularity10 = (
+  payload
+) =>
+  apis.get(
+    API_URLS.GET_RECOMMENDATIONSLIMIT20SEEDGENRESSADMARKET_I_NMINPOPULARITY10,
+    payload
+  );
+
+export const postToken = (payload) =>
+  apis.post(API_URLS.POST_TOKEN, {
+    ...payload,
     headers: {
       "Content-type": "application/x-www-form-urlencoded",
-      Authorization:
-        `Basic {base64(clientId:secretId)}`,
-      ...headers,
+      Authorization: `Basic {base64(clientId:secretId)}`,
+      ...payload?.headers,
     },
-    data,
   });
-};
